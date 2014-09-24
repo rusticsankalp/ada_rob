15a2728c36a3e610923bc96509ffe7d8ce630549
urbansankalp <urbansankalp@outlook.com>
2014-09-23 17:59:26 -0700

    Checkstyle fixes

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,94 +1,93 @@
 public class Percolation {
-    
     private WeightedQuickUnionUF qf;
     private int nN;
     private int max;
     
     private int [][]opened;
     
-    //row =1 col =1 min
-    private int  xyTod(int row , int col)
-    {
-        //since row starts with one and is horizontal each row means n items
-        return (row-1)*nN + col;
-    }
    public Percolation(int N)  // create N-by-N grid, with all sites blocked
    {
        //StdOut.printf("percolation :: N:%d \n",N);
        max = N*N;
        nN = N;
         
-       int topen[][] = new int[N][N];
-       topen[0][0] =0;
        //StdOut.printf("Created new test 2d array \n");
        qf = new WeightedQuickUnionUF(max+2);
        opened = new int[nN][nN];
        //topened[0][0
        //StdOut.printf("Created new 2d array\n");
-       nN= N;
+       nN = N;
 
-       for(int i =0;i<N;i++)
+       for (int i = 0; i < N; i++)
        {
-           for(int j =0 ;j <N; j++)
+           for (int j = 0; j < N; j++)
            {
                //StdOut.printf("percolation :: i:%d , j:%d \n",i,j);
-               opened[i][j] =0;
+               opened[i][j] = 0;
            }
        }
    }
+        
+    //row =1 col =1 min
+    private int  xyTod(int row , int col)
+    {
+        //since row starts with one and is horizontal each row means n items
+        return (row-1)*nN + col;
+    }
+  
    
    //i,j start at 0
    public void open(int i, int j)// open site (row i, column j) if it is not already
    {
        //StdOut.printf("open :: i:%d , j:%d \n",i,j);
-       if(opened[i-1][j-1]==1)
+       if (opened[i-1][j-1] == 1)
        {
            return;
        }
        
-       opened[i-1][j-1]=1;
+       opened[i-1][j-1] = 1;
        
        //left
-       if(j>1)
+       if (j > 1)
        {
-           if(opened[i-1][j-2]==1)
+           if (opened[i-1][j-2] == 1)
            {
-               qf.union(xyTod(i,j),xyTod(i,j-1));
+               qf.union(xyTod(i, j), xyTod(i, j-1));
            }
        }
        
        //right
-       if(j<(nN))
+       if (j < (nN))
        {
-           if(opened[i-1][j]==1)
+           if (opened[i-1][j] == 1)
            {
-               qf.union(xyTod(i,j),xyTod(i,j+1));
+               qf.union(xyTod(i, j), xyTod(i, j+1));
            }
        }
        
        //up 
-       if(i>1)
+       if (i > 1)
        {
-           if(opened[i-2][j-1]==1)
+           if (opened[i-2][j-1] == 1)
            {
-               qf.union(xyTod(i,j),xyTod(i-1,j));
+               qf.union(xyTod(i, j), xyTod(i-1, j));
            }
-       }else if ( i ==1)
+       } else if (i == 1)
        {
-           qf.union(xyTod(i,j),0);
+           qf.union(xyTod(i, j), 0);
        }
        
        //down
-       if(i<nN)
+       if (i < nN)
        {
-           if(opened[i][j-1]==1)
+           if (opened[i][j-1] == 1)
            {
-               qf.union(xyTod(i,j),xyTod(i+1,j));
+               qf.union(xyTod(i, j), xyTod(i+1, j));
            }
        }
-       else if ( i ==nN)
+       else if (i == nN)
        {
-           qf.union(xyTod(i,j),max+1);
+           qf.union(xyTod(i, j), max+1);
            //if((qf.connected(xyTod(i,j),max+1)) && (qf.connected(xyTod(i,j),0)))
              // qf.union(xyTod(i,j),max+1);
            //else
@@ -111,7 +110,7 @@ public class Percolation {
    public boolean isOpen(int i, int j)      // is site (row i, column j) open?
    {
        //StdOut.printf("isOpen :: i:%d , j:%d \n",i,j);
-       boolean res = (opened[i-1][j-1]==1);
+       boolean res = (opened[i-1][j-1] == 1);
        //StdOut.printf("isOpen :: res:%B \n",res);
            
        return res;
@@ -119,13 +118,13 @@ public class Percolation {
    public boolean isFull(int i, int j)      // is site (row i, column j) full?
    {
        //StdOut.printf("isFull :: i:%d , j:%d \n",i,j);
-       boolean res = (qf.connected(xyTod(i,j),0));
+       boolean res = (qf.connected(xyTod(i, j), 0));
        //StdOut.printf("isFull :: res:%B \n",res);
        return res;
    }
    public boolean percolates()              // does the system percolate?
    {
-       return (qf.connected(max+1,0));
+       return (qf.connected(max+1, 0));
    }
    public static void main(String[] args)   // test client, optional
    {
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -2,40 +2,50 @@
 public class PercolationStats {
     
    //Percolation m_Prc;
-   private int n_N,n_T,ind,maxInd;
-   private int ri,rj;
-   private double [] responses;
+   private int nN, nT, ind, maxInd;
+   private int ri, rj;
+   private double []responses;
    
-   private double mean , stddev, clo,chi;
-   public PercolationStats(int N, int T)    // perform T independent computational experiments on an N-by-N grid
+   private double mean , stddev, clo, chi;
+   // perform T independent computational experiments on an N-by-N grid
+   public PercolationStats(int N, int T)    
    {
-       n_N = N;
-       n_T = T;
+       nN = N;
+       nT = T;
        
-       maxInd=n_N*n_N;
-       mean = stddev= clo= chi= 0;
+       maxInd = nN*nN;
+       mean = 0;
+       stddev = 0;
+       clo = 0;
+       chi = 0;
        
        performTest();
-       
-       
        //verify();
    }
-   public double mean()                     // sample mean of percolation threshold
+   
+   // sample mean of percolation threshold
+   public double mean()                     
    {
        //double mean=0;
        return mean;
    }
-   public double stddev()                   // sample standard deviation of percolation threshold
+   
+   // sample standard deviation of percolation threshold
+   public double stddev()                   
    {
        //double sd=0;
        return stddev;
    }
-   public double confidenceLo()             // returns lower bound of the 95% confidence interval
+   
+   // returns lower bound of the 95% confidence interval
+   public double confidenceLo()             
    {
        //double cl=0;
        return clo;
    }
-   public double confidenceHi()             // returns upper bound of the 95% confidence interval
+   
+   // returns upper bound of the 95% confidence interval
+   public double confidenceHi()             
    {
        //double ch=0;
        return chi;
@@ -45,46 +55,48 @@ public class PercolationStats {
    private void performTest()
    {
        int res;
-       responses = new double[n_T];
-       for(int i=0;i<n_T;i++)
+       responses = new double[nT];
+       for (int i = 0;  i < nT; i++)
        {
            res = runTillPercolation();
-           responses[i]=((double)res)/maxInd;
+           responses[i] = ((double) res)/maxInd;
        }
        
        mean = StdStats.mean(responses);
        stddev = StdStats.stddev(responses);
-       double fact = (1.96*stddev)/Math.sqrt(n_T);
+       double fact = (1.96*stddev)/Math.sqrt(nT);
        clo = mean - fact;
        chi = mean + fact;
        
-       StdOut.printf("mean \t = %f \n stddev \t = %f \n 95%% confidence interval \t = %f, %f" ,mean,stddev,clo,chi);
+       StdOut.printf(
+       "mean \t = %f \n stddev \t = %f \n 95%% confidence interval \t = %f, %f"
+        , mean, stddev, clo, chi);
        //clo = StdStats.mean(responses);
        //chi = StdStats.mean(responses);
    }
   
    private void selectUopened(Percolation prc)
    {
-       ri =StdRandom.uniform(n_N)+1;
-       rj = StdRandom.uniform(n_N)+1;
+       ri = StdRandom.uniform(nN) + 1;
+       rj = StdRandom.uniform(nN) + 1;
          
-       while(prc.isOpen(ri,rj))
+       while (prc.isOpen(ri, rj))
        {
-           ri =StdRandom.uniform(n_N)+1;
-           rj = StdRandom.uniform(n_N)+1;
+           ri = StdRandom.uniform(nN) + 1;
+           rj = StdRandom.uniform(nN) + 1;
        }
    }
    private int runTillPercolation()
    {
-     Percolation prc = new Percolation(n_N);
-     int nopened =0;
+     Percolation prc = new Percolation(nN);
+     int nopened = 0;
         
           
-     while(!prc.percolates() && ind <maxInd)
+     while (!prc.percolates() && ind < maxInd)
      {
          //find and open a site
          selectUopened(prc);
-         prc.open(ri,rj);
+         prc.open(ri, rj);
          nopened++;
      }
      
@@ -93,7 +105,8 @@ public class PercolationStats {
    
    public static void main(String[] args)   // test client, described below
    {
-       PercolationStats perS = new PercolationStats(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+       PercolationStats perS = new PercolationStats(Integer.parseInt(args[0]),
+        Integer.parseInt(args[1]));
        StdOut.println(perS.mean());
    }
 }
