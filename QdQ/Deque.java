47c94333984e8ac90027669933e23c486e1c1a27
urbansankalp <urbansankalp@outlook.com>
2014-09-25 16:16:25 -0700

    Initial Deque

--- QdQ/Deque.java
+++ QdQ/Deque.java
@@ -1,11 +1,36 @@
-public class Deque<Item> implements Iterable<Item> {
+public class Deque<Item> //implements Iterable<Item> {
+{
    public Deque()                           // construct an empty deque
+   {
+   }
    public boolean isEmpty()                 // is the deque empty?
+   {
+       return true;
+   }
    public int size()                        // return the number of items on the deque
+   {
+       return 0;
+   }
    public void addFirst(Item item)          // insert the item at the front
+   {
+       
+   }
    public void addLast(Item item)           // insert the item at the end
+   {
+   }
    public Item removeFirst()                // delete and return the item at the front
+   {
+       return null;
+   }
    public Item removeLast()                 // delete and return the item at the end
-   public Iterator<Item> iterator()         // return an iterator over items in order from front to end
+   {
+       return null;
+   }
+//   public Iterator<Item> iterator()         // return an iterator over items in order from front to end
+//   {
+//       return null;
+//   }
    public static void main(String[] args)   // unit testing
+   {
+   }
 }
