3b1f3199d35241f4ba4fdafc45259755a561d0de
Sankalp Gupta <rusticsankalp@outlook.com>
2014-09-25 22:22:33 -0700

    Added some details ,not ready for running

--- QdQ/Deque.java
+++ QdQ/Deque.java
@@ -1,29 +1,59 @@
 public class Deque<Item> //implements Iterable<Item> {
 {
+   private class Node<T>
+   {
+       public Node(T value, Node<T> p, Node<T> n)
+       {
+           prev = p; 
+           next = n;
+       }
+       public T value;
+       public Node<T> prev, next;
+   }
+   
+   private Node<Item> first, last;
+   int size ;
    public Deque()                           // construct an empty deque
    {
+       first = null;
+       last = null;
+       size = 0;
    }
    public boolean isEmpty()                 // is the deque empty?
    {
-       return true;
+       if(size <=0)
+           return true;
    }
    public int size()                        // return the number of items on the deque
    {
-       return 0;
+       return size;
    }
    public void addFirst(Item item)          // insert the item at the front
+   {
+       if(item == null)
+           throw new NullPointerException("attempted to add null in addFirst");
+       
+   }
+   
+   private void add(Node<Item> node, Item item , bool start = true)
    {
        
    }
    public void addLast(Item item)           // insert the item at the end
    {
+       if(item == null)
+           throw new NullPointerException("attempted to add null in addLast");
    }
    public Item removeFirst()                // delete and return the item at the front
    {
+       if(isEmpty())
+           throw new java.util.NoSuchElementException("attempted to remove from emty q in removeFirst");
        return null;
    }
    public Item removeLast()                 // delete and return the item at the end
    {
+       if(isEmpty())
+           throw new java.util.NoSuchElementException("attempted to remove from emty q in removeLast");
        return null;
    }
 //   public Iterator<Item> iterator()         // return an iterator over items in order from front to end
